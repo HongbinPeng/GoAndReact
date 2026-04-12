0a7536d1a552fe6b14cc4be897f3af9e0e7774c6
penghongbin <tom3497737178@gmail.com>
2026-04-12 15:55:10 +0800

    fix:除异常处理

--- week04/practice/basic_12/main.go
+++ week04/practice/basic_12/main.go
@@ -1,19 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func add(a, b float64) float64 {
-	return a + b
+func add(a, b float64) (float64, error) {
+	return a + b, nil
 }
-func sub(a, b float64) float64 {
-	return a - b
+func sub(a, b float64) (float64, error) {
+	return a - b, nil
 }
-func mul(a, b float64) float64 {
-	return a * b
+func mul(a, b float64) (float64, error) {
+	return a * b, nil
 }
-func div(a, b float64) float64 {
-
-	return a / b
+func div(a, b float64) (float64, error) {
+	if b == 0 {
+		return 0, errors.New("除数不能为零")
+	}
+	return a / b, nil
 }
 func main() {
 	var a float64
@@ -26,17 +31,21 @@ func main() {
 	fmt.Scanln(&operator)
 	fmt.Println("请输入第二个数字：")
 	fmt.Scanln(&b)
-	switch operator {
-	case "+":
-		result = add(a, b)
-	case "-":
-		result = sub(a, b)
-	case "*":
-		result = mul(a, b)
-	case "/":
-		result = div(a, b)
-	default:
-		fmt.Println("请输入正确的运算符")
+	operatorfunc := map[string]func(float64, float64) (float64, error){
+		"+": add,
+		"-": sub,
+		"*": mul,
+		"/": div,
+	}
+	opfunc, ok := operatorfunc[operator]
+	if !ok {
+		fmt.Println("无效的运算符")
+		return
+	}
+	result, err := opfunc(a, b)
+	// 处理异常
+	if err != nil {
+		fmt.Printf("发生错误：%s\n", err)
 		return
 	}
 	fmt.Printf("结果是：%.2f\n", result)
